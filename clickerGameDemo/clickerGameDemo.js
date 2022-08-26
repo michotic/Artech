8d4eae678da85f99bee507703c7db0605fb8aa06
Michael Taylor <31018963+michotic@users.noreply.github.com>
2022-08-26 09:56:19 -0300

    Updated cookie clicker game
    - Added saving & loading
    - Added milk based on CPS
    - Adjusted price-scaling
    - Now there can be multiple golden cookies

--- clickerGameDemo/clickerGameDemo.js
+++ clickerGameDemo/clickerGameDemo.js
@@ -1,56 +1,78 @@
-let mainButton;
-let cookieCount = 0;
-let cookieImage;
-let cps = 0; // new -------------------------------------------------------------
-let priceMultiplier = 1.2; // new -----------------------------------------------------------
+let mainButton; // Variable for main clicker/button object
+let cookieCount = 0; // Cookies start at 0
+let cps = 0; // Cookies/second
+let priceMultiplier = 1.07; // Multiplier for store prices after buying an upgrade
 
+// Load game assets
 function preload() {
   cookieImage = loadImage("cookieButton.png");
-  
-  buyButton = loadImage("buyButton.png") // new------------------------------------------
-  storeBackground = loadImage("storeBackground.png"); // new----------------------------------
-  font = loadFont("font.ttf"); // new
-  
+  buyButton = loadImage("buyButton.png") 
+  storeBackground = loadImage("storeBackground.png"); 
+  font = loadFont("font.ttf"); 
   goldCookie = loadImage("goldCookie.png"); // new ----------------------------------------
+  saveImage = loadImage("save.png");
+  loadImg = loadImage("load.png");
 }
 
+// Runs once at game start
 function setup() {
-  textFont(font); // new ---------------------------------------------------------------
-  
+  textFont(font); 
   imageMode(CENTER);
   createCanvas(800, 600);
-  mainButton = new Clicker(170, 300, 150);
-  store = new Store(createVector(width / 2, 50)); // add param-----------------------
-  goldenCookie = new GoldCookie(createVector(-100, -100));
+  mainButton = new Clicker(width / 4.6, 300, 150);
+  store = new Store(createVector(width / 2, 50)); 
+  
+  goldCookies = [];
+  ellipseMode(CENTER);
+  
+  saveButton = new createVector(40, height - 60);
+  rectMode(CENTER);
 }
 
 function draw() {
   background(0, 176, 255);
   mainButton.render();
   fill(255);
+  stroke(0);
+  strokeWeight(4);
   textSize(40);
-  text(cookieCount.toFixed(1) + " Cookies", 125, 50); // new --------------------------------
+  push();
+  textAlign(CENTER);
+  text(float(cookieCount).toFixed(1) + " Cookies", mainButton.position.x, 50); //add str 
+  text(float(cps).toFixed(1) + "/second", mainButton.position.x, 100); //add str
+  pop();
+  cookieCount += cps / 60;
+  store.render(); 
   
-  cookieCount += cps / 60; // new----------------------------------------------------
-  text(cps.toFixed(1) + "/second", 125, 100); // new---------------------------------------------------
-  store.render(); // new-----------------------------------------------------
+  // new
+  fill(255);
+  let milkHeight = constrain((cps / 1000), 0, height / 2);
+  rect(width / 4 - 25,height - (milkHeight / 2), width / 2 - 50, milkHeight);
   
-  // new -------------------------------------------------------------
+  //new-----------------------
   if (random(1) < 0.001) {
-    goldenCookie.position.x = random(width / 2);
-    goldenCookie.position.y = random(height);
-    goldenCookie.remaining = goldenCookie.duration;
-    goldenCookie.reward = random(100, 50000);
+    goldCookies.push(new GoldCookie(createVector(random(width / 2), random(height))));
   }
-  goldenCookie.render();
+  for (let i=0; i < goldCookies.length; i++) {
+    goldCookies[i].render(); 
+    if (goldCookies[i].duration < 0.01) {
+      goldCookies.splice(i, 1);
+    }
+  }
+  
+  let saveSize = 50
+  fill(0);
+  rect(saveButton.x, saveButton.y, saveSize * 1.2, saveSize * 1.2);
+  image(saveImage, saveButton.x, saveButton.y, saveSize, saveSize);
+  rect(saveButton.x + saveSize + 25, saveButton.y, saveSize * 1.2, saveSize * 1.2);
+  image(loadImg, saveButton.x + saveSize + 25, saveButton.y, saveSize, saveSize);
 }
 
 function mousePressed() {
-  if (dist(mouseX, mouseY, mainButton.position.x, mainButton.position.y) < mainButton.radius) {
+  if (dist(mouseX, mouseY, mainButton.position.x, mainButton.position.y) < mainButton.radius / 2) {
     cookieCount = mainButton.addCookies(cookieCount, mainButton.perClick);
   }
   
-  // new ------------------------------------------------------------------------------------------------------------
   for (let i = 0; i < store.buttons.length; i++) {
     let bX = store.buttons[i].x;
     let bY = store.buttons[i].y;
@@ -64,8 +86,33 @@ function mousePressed() {
     }
   }
   
-  if (dist(mouseX, mouseY, goldenCookie.position.x, goldenCookie.position.y) < goldenCookie.radius / 2) {
-    cookieCount += goldenCookie.reward;
-    goldenCookie.position.x = -100;
+  // new --------------------------------------------
+  for (let i = 0; i < goldCookies.length; i++) {
+    let goldenCookie = goldCookies[i];
+    if (dist(mouseX, mouseY, goldenCookie.position.x, goldenCookie.position.y) < goldenCookie.radius / 2) {
+      cookieCount += goldCookies[i].reward;
+      goldenCookie.duration=0;
+    }
+  }
+  
+  if (dist(mouseX, mouseY, saveButton.x, saveButton.y) < 30) {
+    let saveInfo = [int(cookieCount), int(cps)];
+    for (let i=0; i < store.items.length; i++) {
+      saveInfo.push(store.items[i]["price"]);
+    }
+    print(saveInfo);
+    saveStrings(saveInfo, 'save.txt');
+  }
+  if (dist(mouseX, mouseY, saveButton.x + 75, saveButton.y) < 30) {
+    loadStrings('save.txt', loadGame);
+  }
+}
+
+function loadGame(data) {
+  cookieCount = float(data[0]);
+  cps = float(data[1]);
+  for (let i = 0; i < store.items.length; i++) {
+    store.items[i]["price"] = float(data[i+2]);
+    print(store.items[i]["name"]);
   }
 }
--- clickerGameDemo/goldCookie.js
+++ clickerGameDemo/goldCookie.js
@@ -3,15 +3,13 @@ class GoldCookie {
     this.position = position;
     this.radius = 50;
     this.duration = 60 * 5;
-    this.remaining = 0;
-    this.reward = 0;
+    this.reward = (cps + 1) * random(10, 100);
   }
   
   render() {
+    fill(0);
+    ellipse(this.position.x, this.position.y, this.radius * 1.2, this.radius * 1.2);
     image(goldCookie, this.position.x, this.position.y, this.radius, this.radius);
-    this.remaining -= 1;
-    if (this.remaining < 1) {
-      this.position.x = -100;
-    }
+    this.duration -= 1;
   }
 }
